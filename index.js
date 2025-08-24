041c98c31d011ae61f0c55834fa7de491d41d258
VadimMG310865 <vad3108@mail.ru>
2025-08-24 15:29:26 +0300

    my commit_1

---
+++ index.js
@@ -0,0 +1,12 @@
+// Release 0
+const student = {};
+student.firstName = "Petr"; 
+student.lastName = "Perviy";
+student.firstName = "Petya";
+//delete student.firstName;
+// Release 1
+const group = [];
+group.push(student);
+group.push(student_1= {firstName:"Ekaterina", lastName:"Pervaya"}, student_2= {firstName:"Nikolay", lastName:"Vtoroy"} );
+
+console.log (group);
